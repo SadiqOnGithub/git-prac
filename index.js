e8d816bd2875e5091c1587ac800f5aaa50a8dcce
Syed Sadiq Ali - wsl <sadiqonemail@gmail.com>
2023-02-08 21:37:14 +0530

    command used

--- index.js
+++ index.js
@@ -4,12 +4,19 @@
 // 4
 // 5
 
-// command used
+
+// =======================
+// COMMAND USED
+// =======================
 // git checkout (#hash)					->	checking out
 // git reset --hard HEAD
 // git reset --hard (#hash) 		-> 	will reset the working directory to particular commit
+//																		and commit history are also restored to that particular
 // git reset --hard HEAD~n   		-> 	where n refer to parent commit
 // git reset --hard HEAD@{n}  	-> 	where n refer to reflog HEAD
+
+// git reset HEAD		  					-> 	commit history are restored 
+// 																		but working directory changes are not lost
 
 // git switch - 								->	shorthand to go bach to last branch while in detached HEAD mode
 
