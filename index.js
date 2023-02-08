fde5f9a8f9d49e79a2e6ab6dd2f691d85fbf0f02
root <root@Workstation>
2023-02-08 21:05:41 +0530

    command used

--- index.js
+++ index.js
@@ -3,3 +3,10 @@
 // 3
 // 4
 // 5
+
+// command used
+// git checkout (#hash)					->	checking out 
+// git reset --hard HEAD
+// git reset --hard (#hash) 		-> 	will reset the working directory to particular commit
+// git reset --hard HEAD~n   		-> 	where n refer to parent commit
+// git reset --hard HEAD@{n}  	-> 	where n refer to reflog HEAD
