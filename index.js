d1080fa820dd0c2288f0788c74a56bae72172692
Syed Sadiq Ali - wsl <sadiqonemail@gmail.com>
2023-02-08 21:29:09 +0530

    command used

--- index.js
+++ index.js
@@ -5,8 +5,12 @@
 // 5
 
 // command used
-// git checkout (#hash)					->	checking out 
+// git checkout (#hash)					->	checking out
 // git reset --hard HEAD
 // git reset --hard (#hash) 		-> 	will reset the working directory to particular commit
 // git reset --hard HEAD~n   		-> 	where n refer to parent commit
 // git reset --hard HEAD@{n}  	-> 	where n refer to reflog HEAD
+
+// git switch - 								->	shorthand to go bach to last branch while in detached HEAD mode
+
+// git config --global alias.graph "log --oneline --all --graph"			-> setting alias
